3c3e9e971f40acc2dd97dbbd96af6ef681142cf5
Krzysztof Galgan <galganek.krzys@gmail.com>
2025-10-28 14:06:15 +0100

    6.6

---
+++ 02-TypesAndVariables/6/6.2.py
@@ -0,0 +1,6 @@
+###
+# A program that prints your initials
+#
+name = 'George'
+surname = 'May'
+print(name[0], '', surname[0])
---
+++ 02-TypesAndVariables/6/6.5.py
@@ -0,0 +1,9 @@
+#Enter phone number: 543097329
+#Phone number: 543-097-329
+###
+# a program that prints a 9-digit telephone number
+# entered from the keyboard as three groups of 3 digits each,
+# separated by a dash character.
+#
+phone = input('Enter phone number: ')
+print(f'More readable: {phone[0:3]}-{phone[3:6]}-{phone[6:9]}')
